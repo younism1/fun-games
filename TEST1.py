446b8fcc7104b9d4bce12ede30fd339b73fd73e3
Mo Younis <younism@recruitment.rul>
2018-02-12 13:38:21 +0000

    First Attempt

---
+++ TEST1.py
@@ -0,0 +1,12 @@
+print ("hello world")
+print (7*2)
+print (3+3)
+print ()
+print ("the end")
+print ("python's strings are easy to use")
+print ("we can include 'quotes' in string")
+print ("hello"+"world")
+
+
+array = ["Hey", 1]
+print(array[0])
