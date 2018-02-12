61c59457d89ff4d20f7500bb07f2c2bce77f526c
Mo Younis <younism@recruitment.rul>
2018-02-12 13:44:27 +0000

    second version

--- TEST1.py
+++ TEST1.py
@@ -1,9 +1,9 @@
 print ("hello world")
 print (7*2)
-print (3+3)
+print (3+4)
 print ()
 print ("the end")
-print ("python's strings are easy to use")
+print ("python's strings are easy to use!")
 print ("we can include 'quotes' in string")
 print ("hello"+"world")
 
